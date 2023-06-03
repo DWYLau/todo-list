6ee60eabc2d614b1f0ff56becc92d1f457316fbf
Derek <dereklau15@gmail.com>
2023-06-03 15:07:44 +0900

    Create working vertical tabs with active classes

--- src/buttonController.js
+++ src/buttonController.js
@@ -12,23 +12,22 @@ closeBtn.addEventListener("click", function () {
   document.getElementById("main").style.marginLeft = "0";
 });
 
-function openTab(evt, tabName) {
-  // Declare all variables
-  let i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+function openTabs() {
+  let tabs = document.querySelectorAll(".tablinks");
+  let tabContents = document.querySelectorAll(".tabcontent");
+  console.log(tabs);
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabContents.forEach((content) => {
+        content.classList.remove("active");
+      });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      tabContents[index].classList.add("active");
+      tabs[index].classList.add("active");
+    });
+  });
 }
+
+openTabs();
