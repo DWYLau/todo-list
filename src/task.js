9ec2a97258cc93df29d2f12e399226a608e8928e
Derek <dereklau15@gmail.com>
2023-06-21 10:52:13 +0900

    Finish filter date logic

--- src/task.js
+++ src/task.js
@@ -1,5 +1,4 @@
 import { isWithinInterval, parseISO, format } from "date-fns";
-import { beTarask } from "date-fns/locale";
 
 let myTasks = [];
 
@@ -24,90 +23,81 @@ function createTask() {
     date.value
   );
   myTasks.push(task);
-  appendToAllTask();
+  appendToProjects();
 }
 
-function appendToAllTask() {
+function appendToProjects() {
   const allTasksTab = document.getElementById("alltasks");
-  const tasksDivs = allTasksTab.querySelectorAll("div");
-  tasksDivs.forEach((div) => div.remove());
-  createCard(allTasksTab);
-}
+  const allTasksDivs = allTasksTab.querySelectorAll("div");
+  allTasksDivs.forEach((div) => div.remove());
 
-function createCard(tab) {
+  const todayTab = document.getElementById("today");
+  const todayTaskDivs = todayTab.querySelectorAll("div");
+  todayTaskDivs.forEach((div) => div.remove());
+
+  const nextSevenTab = document.getElementById("nextseven");
+  const nextSevenTaskDivs = nextSevenTab.querySelectorAll("div");
+  nextSevenTaskDivs.forEach((div) => div.remove());
   myTasks.forEach((task) => {
-    const card = document.createElement("div");
-    tab.appendChild(card);
-    card.classList.add("card");
-
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.classList.add("checkbox");
-
-    const title = document.createElement("p");
-    title.classList.add("title");
-    const description = document.createElement("p");
-    description.classList.add("description");
-    const priority = document.createElement("p");
-    priority.classList.add("priority");
-    const date = document.createElement("p");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("deletebtn");
-
-    card.appendChild(checkbox);
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(priority);
-    card.appendChild(date);
-    card.appendChild(deleteBtn);
-
-    title.textContent = task.title;
-    description.textContent = task.description;
-    priority.textContent = task.priority;
-    priority.style.background = "green";
-    date.textContent = task.dueDate;
-    deleteBtn.textContent = "X";
-
+    createCard(
+      allTasksTab,
+      task.title,
+      task.description,
+      task.priority,
+      task.dueDate
+    );
     if (checkDate(task.dueDate) === true) {
-      const tabContent = document.getElementById("today");
-      const tasksDivs = tabContent.querySelectorAll("div");
-      tasksDivs.forEach((div) => div.remove());
-
-      const card = document.createElement("div");
-      tabContent.appendChild(card);
-      card.classList.add("card");
-
-      const checkbox = document.createElement("input");
-      checkbox.setAttribute("type", "checkbox");
-      checkbox.classList.add("checkbox");
-
-      const title = document.createElement("p");
-      title.classList.add("title");
-      const description = document.createElement("p");
-      description.classList.add("description");
-      const priority = document.createElement("p");
-      priority.classList.add("priority");
-      const date = document.createElement("p");
-      const deleteBtn = document.createElement("button");
-      deleteBtn.classList.add("deletebtn");
-
-      card.appendChild(checkbox);
-      card.appendChild(title);
-      card.appendChild(description);
-      card.appendChild(priority);
-      card.appendChild(date);
-      card.appendChild(deleteBtn);
-
-      title.textContent = task.title;
-      description.textContent = task.description;
-      priority.textContent = task.priority;
-      priority.style.background = "green";
-      date.textContent = task.dueDate;
-      deleteBtn.textContent = "X";
-    } else {
-      return false;
+      createCard(
+        todayTab,
+        task.title,
+        task.description,
+        task.priority,
+        task.dueDate
+      );
+    } else if (checkDate(task.dueDate) === false) {
+      createCard(
+        nextSevenTab,
+        task.title,
+        task.description,
+        task.priority,
+        task.dueDate
+      );
     }
   });
+}
+
+function createCard(tab, taskTitle, taskDesc, taskPriority, taskDate) {
+  const card = document.createElement("div");
+  tab.appendChild(card);
+  card.classList.add("card");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.classList.add("checkbox");
+
+  const title = document.createElement("p");
+  title.classList.add("title");
+  const description = document.createElement("p");
+  description.classList.add("description");
+  const priority = document.createElement("p");
+  priority.classList.add("priority");
+  const date = document.createElement("p");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deletebtn");
+
+  card.appendChild(checkbox);
+  card.appendChild(title);
+  card.appendChild(description);
+  card.appendChild(priority);
+  card.appendChild(date);
+  card.appendChild(deleteBtn);
+
+  title.textContent = taskTitle;
+  description.textContent = taskDesc;
+  priority.textContent = taskPriority;
+  priority.style.background = "green";
+  date.textContent = taskDate;
+  deleteBtn.textContent = "X";
 }
 
 function checkDate(date) {
@@ -116,15 +106,16 @@ function checkDate(date) {
   let month = today.getMonth();
   let year = today.getFullYear();
   let formatDate = format(new Date(year, month, day), "yyyy-MM-dd");
-
   const nextSevenDays = new Date(new Date().setDate(new Date().getDate() + 7));
+  const withinDates = isWithinInterval(parseISO(date), {
+    start: today,
+    end: nextSevenDays,
+  });
 
   if (date === formatDate) {
     return true;
-  } else {
-    console.log("NO MATCH");
-    console.log(date);
-    console.log(formatDate);
+  } else if (withinDates === true) {
+    return false;
   }
 }
 
